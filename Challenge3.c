50b46976a0951a41434319c0c3c59a7b06159e1e
User <user@example.com>
2025-09-02 12:03:45 +0100

    Ajout du programme pour convertir la distance de kilomètres en yards

--- Challenge3.c
+++ Challenge3.c
@@ -3,14 +3,11 @@
 int main() {
     float km, yards;
 
-    // إدخال المسافة بالكيلومتر
     printf("Entrez la distance en kilomtres : ");
     scanf("%f", &km);
 
-    // تحويل الكيلومتر إلى Yards
     yards = km * 1093.61;
 
-    // عرض النتيجة
     printf("%.2f km = %.2f yards\n", km, yards);
 
     return 0;
