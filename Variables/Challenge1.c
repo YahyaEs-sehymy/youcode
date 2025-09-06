00eef58832177c821c8bc7185375a72d1e8409f2
User <user@example.com>
2025-09-06 12:13:23 +0100

    Ajout du programme de saisie et affichage des informations personnelles

---
+++ Variables/Challenge1.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+int main() {
+    char nom[50];
+    char prenom[50];
+    int age;
+    char sexe[10];
+    char email[100];
+
+    printf("Entrez votre nom : ");
+    scanf("%s", nom);
+
+    printf("Entrez votre prenom : ");
+    scanf("%s", prenom);
+
+    printf("Entrez votre age : ");
+    scanf("%d", &age);
+
+    printf("Entrez votre sexe (H/F) : ");
+    scanf("%s", sexe);
+
+    printf("Entrez votre email : ");
+    scanf("%s", email);
+
+    printf("\n Informations Personnelles\n");
+    printf("Nom       : %s\n", nom);
+    printf("Prenom    : %s\n", prenom);
+    printf("Age       : %d\n", age);
+    printf("Sexe      : %s\n", sexe);
+    printf("Email     : %s\n", email);
+
+    return 0;
+}
+
+
+
