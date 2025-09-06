86df1f44655820a935364bb8691c1fef4848eea2
User <user@example.com>
2025-09-06 12:17:01 +0100

    Ajout du programme de conversion kilomètres vers yards

---
+++ Variables/Challenge3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float km, yards;
+
+    printf("Entrez la distance en kilomtres : ");
+    scanf("%f", &km);
+
+    yards = km * 1093.61;
+
+    printf("%.2f km = %.2f yards\n", km, yards);
+
+    return 0;
+}
